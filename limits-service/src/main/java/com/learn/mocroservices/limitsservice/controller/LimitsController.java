0a2babd85bbc6b4abd3ab6f9a9667dc18a68ccb0
Vishal Singh Yadav <vishal637yadav@gmail.com>
2024-03-28 17:44:28 +0530

    Initial Commit

---
+++ limits-service/src/main/java/com/learn/mocroservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,21 @@
+package com.learn.mocroservices.limitsservice.controller;
+
+import com.learn.mocroservices.limitsservice.bean.Limits;
+import com.learn.mocroservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+        //return new Limits(1,1000);
+    }
+
+}
