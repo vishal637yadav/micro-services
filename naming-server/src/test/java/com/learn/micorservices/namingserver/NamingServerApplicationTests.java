d8d1ff6c0aa02cc07461a4c13ded273393775dad
Vishal Singh Yadav <vishal637yadav@gmail.com>
2024-03-29 13:00:27 +0530

    Registering the MicroServices with Eureka Naming Server

---
+++ naming-server/src/test/java/com/learn/micorservices/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.micorservices.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
