85d9c2d9e62b22f6636d1b46021435058c1e36d3
Vishal Singh Yadav <vishal637yadav@gmail.com>
2024-03-29 20:38:50 +0530

    Resilience 4j

--- currency-exchange-service/src/main/java/com/learn/micorservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/learn/micorservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,5 +1,7 @@
 package com.learn.micorservices.currencyexchangeservice;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,13 +16,16 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @Retry(name="sample-api", fallbackMethod = "hardcodedResponse")
+    //@Retry(name="sample-api", fallbackMethod = "hardcodedResponse")
+    //@CircuitBreaker(name="default", fallbackMethod = "hardcodedResponse")
+    //10s --> 10000 calls to the sample api
+    @RateLimiter(name="default")
     public String sampleApi(){
         logger.info("Sample Apicall received");
-
+/*
         ResponseEntity<String> responseEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
                 String.class);
-
+*/
         return "Sample Api";
     }
 
