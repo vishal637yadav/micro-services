09bc6620a1f30d55f3bbfaade3a7d89044f9f3a4
Vishal Singh Yadav <vishal637yadav@gmail.com>
2024-03-29 13:08:51 +0530

    Changes in CurrencyConversionProxy to enable LoadBalancing

--- currency-conversion-service/src/main/java/com/learn/micorservices/currencyconversionservice/CurrencyConversionProxy.java
+++ currency-conversion-service/src/main/java/com/learn/micorservices/currencyconversionservice/CurrencyConversionProxy.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url="localhost:8000")
+//@FeignClient(name = "currency-exchange-service", url="localhost:8000")
+@FeignClient(name = "currency-exchange-service")
 public interface CurrencyConversionProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
